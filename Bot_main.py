ccbe76cbabea3c32d518d6d053a5bba85fa0612f
StoKer <bramrekots@gmail.com>
2022-02-21 22:04:37 -0300

    Bot_main.py
    Add Time stamp on prints
    removed thermocolor
    correction of some detector positions

--- Bot_main.py
+++ Bot_main.py
@@ -1,10 +1,14 @@
 import pyautogui
 import keyboard
 import time
-from termcolor import colored
+#from termcolor import colored
+from datetime import datetime
 
 
 
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
 
 def click_event(x, y):
     
@@ -15,7 +19,8 @@ def click_event(x, y):
 def draw_card():
     
     if pyautogui.pixelMatchesColor(1530, 1001, (179, 167, 135)):
-        print(colored('[LOG]', 'yellow'), colored('Draw Card', 'blue'))
+        #print(colored('[LOG]', 'yellow'), colored('Draw Card', 'blue'))
+        print((current_time), ('[LOG] Draw Card'))
         time.sleep(0.5)
         click_event(1900, 480)
         click_event(1900, 480)
@@ -30,13 +35,17 @@ def draw_card():
 def botao_azul():
 
     if pyautogui.pixelMatchesColor(1480, 439, (80, 147, 147)):
-        print(colored('[LOG]', 'yellow'), colored('Player Phase', 'blue'))
+        #print(colored('[LOG]', 'yellow'), colored('Player Phase', 'blue'))
+        print((current_time), ('[LOG] Player Phase'))
+
         time.sleep(0.5)
         click_event(1480, 439)
-        print(colored('[LOG]', 'yellow'), colored('skipping phase', 'blue'))
+        #print(colored('[LOG]', 'yellow'), colored('skipping phase', 'blue'))
+        print((current_time), ('[LOG] skipping phase'))
         time.sleep(0.5)
         click_event(1506, 780)
-        print(colored('[LOG]', 'yellow'), colored('skipped phase...', 'blue')) 
+        #print(colored('[LOG]', 'yellow'), colored('skipped phase...', 'blue')) 
+        print((current_time), ('[LOG] skipped phase...'))
         if pyautogui.pixelMatchesColor(965, 507, (62, 92, 127)):
             botao_azul()
            
@@ -47,19 +56,24 @@ def botao_azul():
 def discart_card():
     
     if pyautogui.pixelMatchesColor(1391, 604, (59, 159, 247)):
-        print(colored('[LOG]', 'yellow'), colored('Card To Discard', 'blue'))
+        #print(colored('[LOG]', 'yellow'), colored('Card To Discard', 'blue'))
+        print((current_time), ('[LOG] Card To Discard...'))
         time.sleep(0.5)
         click_event(588, 838)
-        print(colored('[LOG]', 'yellow'), colored('Selecting card', 'blue'))
+        #print(colored('[LOG]', 'yellow'), colored('Selecting card', 'blue'))
+        print((current_time), ('[LOG] Selecting card...'))
         time.sleep(0.5)
         click_event(968, 1020)
-        print(colored('[LOG]', 'yellow'), colored('discarded card', 'blue'))
+        #print(colored('[LOG]', 'yellow'), colored('discarded card', 'blue'))
+        print((current_time), ('[LOG] Discarded card...'))
 
 def botao_vermelho():
     
     if pyautogui.pixelMatchesColor(1510, 439, (151, 22, 17)):
-        print(colored('[LOG]', 'yellow'), colored('Enemy turn', 'red'))
-        print(colored('[LOG]', 'yellow'), colored('Wait...', 'red'))
+        #print(colored('[LOG]', 'yellow'), colored('Enemy turn', 'red'))
+        print((current_time), ('[LOG] Enemy turn'))
+        #print(colored('[LOG]', 'yellow'), colored('Wait...', 'red'))
+        print((current_time), ('[LOG] Wait...'))
         time.sleep(5)
     else:
         botao_azul()
@@ -68,7 +82,8 @@ def botao_vermelho():
 def screen_defeat():
     
     if pyautogui.pixelMatchesColor(1418, 791, (223, 12, 62)):
-        print(colored('[LOG]', 'yellow'), colored('Defeat screen', 'magenta'))
+        #print(colored('[LOG]', 'yellow'), colored('Defeat screen', 'magenta'))
+        print((current_time), ('[LOG] Defeat screen'))
         time.sleep(2)
         click_event(964, 971)
     else:
@@ -78,7 +93,8 @@ def screen_defeat():
 def screen_duelpass():
     
     if pyautogui.pixelMatchesColor(668, 268, (191, 193, 191)):
-        print(colored('[LOG]', 'yellow'), colored('skipping Duel Pass', 'magenta'))
+        #print(colored('[LOG]', 'yellow'), colored('skipping Duel Pass', 'magenta'))
+        print((current_time), ('[LOG] skipping Duel Pass'))
         time.sleep(2)
         click_event(1590, 993)
         time.sleep(3)
@@ -91,7 +107,8 @@ def screen_duelpass():
 def screen_duelresults():
     
     if pyautogui.pixelMatchesColor(42, 61, (48, 48, 48)):
-        print(colored('[LOG]', 'yellow'), colored('skipping Duel Results', 'magenta'))
+        #print(colored('[LOG]', 'yellow'), colored('skipping Duel Results', 'magenta'))
+        print((current_time), ('[LOG] skipping Duel Results'))       
         time.sleep(2)
         pyautogui.moveTo(100, 100)
         pyautogui.click(button='right')
@@ -101,19 +118,14 @@ def screen_duelresults():
         pyautogui.moveTo(1900, 10)
         pyautogui.click(button='right')
 
-def screen_XYZEvent():
-    
-    if pyautogui.pixelMatchesColor(1685, 518, (26, 32, 5)):
-        print(colored('[LOG]', 'yellow'), colored('Receiving Rewards', 'magenta'))
-        time.sleep(2)
-        pyautogui.moveTo(1900, 10)
-        pyautogui.click(button='right')
-
 
 def button_duel():
     
     if pyautogui.pixelMatchesColor(1288, 854, (203, 253, 0)):
-        print(colored('[LOG]', 'yellow'), colored('Clicking Start Another Duel', 'magenta'))
+        #print(colored('[LOG]', 'yellow'), colored('Start Another Duel', 'magenta'))
+        print((current_time), ('[LOG] Start Another Duel'))
+        time.sleep(2)
+        click_event(964, 788 )
         time.sleep(2)
         click_event(1590, 517)
         time.sleep(1)
@@ -124,7 +136,8 @@ def button_duel():
 def window_nocard():
     
     if pyautogui.pixelMatchesColor(1356, 374, (59, 159, 247)):
-        print(colored('[LOG]', 'yellow'), colored('Window no cards to apply effects', 'magenta'))
+        #print(colored('[LOG]', 'yellow'), colored('Window no cards to apply effects', 'magenta'))
+        print((current_time), ('[LOG] Window no cards to apply effects'))
         time.sleep(1)
         click_event(964, 589)
     else:
@@ -133,7 +146,8 @@ def window_nocard():
 def window_enemy_card():
     
     if pyautogui.pixelMatchesColor(645, 774, (163, 26, 26)):
-        print(colored('[LOG]', 'yellow'), colored('Any enemy card to selected', 'magenta'))
+        #print(colored('[LOG]', 'yellow'), colored('Any enemy card to selected', 'magenta'))
+        print((current_time), ('[LOG] Any enemy card to selected'))
         time.sleep(1)
         click_event(671, 840) #Select card
         time.sleep(1)
@@ -145,7 +159,8 @@ def window_enemy_card():
 def card_01():
     
     if pyautogui.pixelMatchesColor(1284, 605, (59, 159, 247)):
-        print(colored('[LOG]', 'yellow'),('[CARD EFFECT]', 'green'), colored('Hand destruction', 'cyan'))
+        #print(colored('[LOG]', 'yellow'),colored('[CARD EFFECT]', 'green'), colored('Hand destruction', 'cyan'))
+        print((current_time), ('[LOG] [CARD EFFECT] Hand destruction')) 
         time.sleep(1)
         click_event(669, 805)
         time.sleep(2)
@@ -160,7 +175,8 @@ def card_01():
 def card_02():
     
     if pyautogui.pixelMatchesColor(752, 787, (171, 171, 171)):
-        print(colored('[LOG]', 'yellow'),('[CARD EFFECT]', 'green'), colored('Toon page flip', 'cyan'))
+        #print(colored('[LOG]', 'yellow'),colored('[CARD EFFECT]', 'green'), colored('Toon page flip', 'cyan'))
+        print((current_time), ('[LOG] [CARD EFFECT] Toon page flip'))
         time.sleep(1)
         click_event(813, 870)
         time.sleep(2)
@@ -172,7 +188,8 @@ def card_02():
 def coin():
     
     if pyautogui.pixelMatchesColor(624, 823, (203, 253, 0)):
-        print(colored('[LOG]', 'yellow'), colored('Coin Win, going first', 'magenta'))
+        #print(colored('[LOG]', 'yellow'), colored('Coin game Win, going first', 'magenta'))
+        print((current_time), ('[LOG] Coin game Win, going first'))
         time.sleep(1)
         click_event(767, 839)
     else:
@@ -187,7 +204,6 @@ while keyboard.is_pressed('q') == False:
     screen_defeat()
     screen_duelpass()
     screen_duelresults
-    screen_XYZEvent
     button_duel()
     window_nocard
     window_enemy_card()
